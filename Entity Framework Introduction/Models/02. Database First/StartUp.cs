de69e3ddb6cf4e03dc4a92f2fa492d4408a02c67
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-10-23 16:09:31 +0300

    Add files via upload

---
+++ Entity Framework Introduction/Models/02. Database First/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using SoftUni.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context)) ;
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => x).OrderBy(x => x.EmployeeId).ToList();
+            List<string> emp = new List<string>();
+            foreach (var employee in employees)
+            {
+                emp.Add($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+                //Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return string.Join("\n", emp);
+        }
+    }
+    //Scaffold-DbContext -Connection "Server=DESKTOP-6RL5K65;Database=SoftUni;Integrated Security=True;TrustServerCertificate=True;" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data/Models
+}
