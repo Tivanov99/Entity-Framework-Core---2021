6447f7ac3317b65f1ba1e5598ee71c2bea878c56
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-10-31 22:33:13 +0200

    Add files via upload

---
+++ Entity Relations/3. Football Betting/Models/Homework.cs
@@ -0,0 +1,34 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("HomeworkSubmissions")]
+    public class Homework
+    {
+        [Key]
+        public int HomeWorkId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public ContentType ContentType { get; set; }
+
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(StudentId))]
+        public Student Student { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; }
+    }
+}
---
+++ Entity Relations/3. Football Betting/Models/StudentCourse.cs
@@ -0,0 +1,19 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+
+    [Table("CourseEnrollments")]
+    public class StudentCourse
+    {
+        public int StudentId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Student Student { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
