a74443287d3cd66ed791285bbbc41235f23730dc
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-10-31 12:59:27 +0200

    Add files via upload

---
+++ Entity Relations/1. Student System/Resource.cs
@@ -0,0 +1,28 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Resources")]
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarcar(50)")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; }
+    }
+}
