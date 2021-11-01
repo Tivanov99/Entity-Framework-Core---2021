8f14e49394df6ef857adf2d06ff26bd2ce4c298b
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-01 21:30:24 +0200

    Add files via upload

---
+++ Entity Relations/4.Football Betting Updating/Models/Course.cs
@@ -0,0 +1,47 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Courses")]
+    public class Course
+    {
+
+        public Course()
+        {
+            Resources = new HashSet<Resource>();
+
+            StudentsEnrolled = new HashSet<StudentCourse>();
+
+            HomeworkSubmissions = new HashSet<Homework>();
+        }
+
+        [Key]
+        public int CourseId { get; set; }
+
+        //TODO: check unicode
+        [Required]
+        [DataType("nvarchar(80)")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public Decimal Price { get; set; }
+
+        public ICollection<Resource> Resources { get; set; }
+
+        public ICollection<Homework> HomeworkSubmissions { get; set; }
+
+        public ICollection<StudentCourse> StudentsEnrolled { get; set; }
+    }
+}
