3ad6a77ca8b67cc53cf2b97e3d67d2b7e249be6d
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-04 21:31:39 +0200

    Add files via upload

---
+++ Entity Relations/1. Student System/Validations/ConstantValidatons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Validations
+{
+    static class ConstantValidatons
+    {
+        //student
+        public const int StudentNameMaxLength = 100;
+
+        public const int PhonenumberLength = 10;
+
+        //course
+        public const int CourseNameMaxLength = 80;
+
+        //resource
+        public const int ResourceNameMaxLength = 50;
+        public const int UrlMaxLength = 3000;
+    }
+}
