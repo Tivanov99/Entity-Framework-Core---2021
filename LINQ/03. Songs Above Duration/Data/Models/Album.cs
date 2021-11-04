71cfaac116ff0cb8005f4d58ef6422d67e2e98f9
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-04 09:56:40 +0200

    Add files via upload

---
+++ LINQ/03. Songs Above Duration/Data/Models/Album.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        //TODO: the sum of all song prices in the album 
+        public Decimal Price => CalculateSongsPrices();
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public virtual Producer Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+
+        private decimal CalculateSongsPrices()
+        {
+            decimal sum = 0;
+            foreach (var item in Songs)
+            {
+                sum += item.Price;
+            }
+            return sum;
+        }
+    }
+}
---
+++ LINQ/03. Songs Above Duration/Data/Models/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Writer
+    {
+        public Writer()
+        {
+            Songs = new HashSet<Song>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Pseudonym { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
