e4d5fec1b6548e059c713f0eb254123080b0ed2a
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-04 21:29:20 +0200

    Add files via upload

---
+++ Entity Relations/4. Student System Update/Data/Models/Resource.cs
@@ -0,0 +1,27 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using P01_StudentSystem.Enums;
+    using P01_StudentSystem.Validations;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [MaxLength(ConstantValidatons.ResourceNameMaxLength)]
+        [DataType("nvarchar")]
+        public string Name { get; set; }
+
+        [MaxLength(ConstantValidatons.UrlMaxLength)]
+        [DataType("varchar")]
+        public string Url { get; set; }
+
+        public ResourceTypes ResourceType { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Entity Relations/4. Student System Update/Data/Models/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class StudentCourse
+    {
+        //TODO:Make foreign key and Composite Primary key, add naviagitons properties
+        [ForeignKey(nameof(Student))]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
