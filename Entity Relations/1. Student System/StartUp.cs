90eff8d2a7ea8264b83ea69e38631b00371de2db
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-04 21:32:28 +0200

    Add files via upload

---
+++ Entity Relations/1. Student System/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StudentSystemContext context = new StudentSystemContext();
+
+            if (context.Database.EnsureCreated())
+            {
+                Console.WriteLine("Db was created!");
+            }
+        }
+    }
+}
