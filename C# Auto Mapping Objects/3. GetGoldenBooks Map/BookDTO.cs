4c6a3c1c6465319d36d4817d6104f03a13214522
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-07 21:49:15 +0200

    Add files via upload

---
+++ C# Auto Mapping Objects/3. GetGoldenBooks Map/BookDTO.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BookShop.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+    }
+}
