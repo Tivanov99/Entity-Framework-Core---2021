62eb38ad8038cefd68ee472646e9225ed31ae566
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-07 22:21:44 +0200

    Add files via upload

---
+++ C# Auto Mapping Objects/5. MapBooks/MapperProfiles/BookInfoDTO.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.MapperProfiles
+{
+    public class BookInfoDTO : Profile
+    {
+        public BookInfoDTO()
+        {
+            this.CreateMap<Book, BookInfoDTO>()
+                .ForMember(b => b.AuthorFullName,
+                             p => p.MapFrom(
+                                 b => $"{b.Author.FirstName} {b.Author.LastName}")
+                             );
+        }
+
+        public string AuthorFullName { get; set; }
+
+        public string Title { get; set; }
+    }
+}
