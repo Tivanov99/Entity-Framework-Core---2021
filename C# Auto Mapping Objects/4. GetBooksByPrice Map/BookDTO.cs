6475584b902b774541f5badfc88c4c818fb07903
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-07 21:53:48 +0200

    Add files via upload

---
+++ C# Auto Mapping Objects/4. GetBooksByPrice Map/BookDTO.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookShop.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
