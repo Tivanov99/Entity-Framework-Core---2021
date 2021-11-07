f45e04d2ec9ae5c508b7e6774a8b311b7a04c11a
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-07 21:32:11 +0200

    Add files via upload

---
+++ C# Auto Mapping Objects/MapBooks/BookDTO.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
