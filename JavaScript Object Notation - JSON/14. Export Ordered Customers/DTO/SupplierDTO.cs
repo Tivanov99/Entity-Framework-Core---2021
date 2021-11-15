753e1f8342de17ab72ec7319b1b8ef037c3f1129
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-16 01:00:53 +0200

    Add files via upload

---
+++ JavaScript Object Notation - JSON/14. Export Ordered Customers/DTO/SupplierDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class SupplierDTO
+    {
+        public int Id { get; set; }
+    }
+}
