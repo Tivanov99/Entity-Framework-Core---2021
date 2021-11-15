127278aebc8d2d2216c844dcb48e2c155f583209
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-15 21:14:53 +0200

    Add files via upload

---
+++ JavaScript Object Notation - JSON/06. Export Sold Products/ProductShopProfile.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+    using ProductShop.DTO_S;
+    using ProductShop.Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<Product, ProductDTO>()
+                .ForMember(dest => dest.Seller, y => y.MapFrom(src => $"{src.Seller.FirstName} {src.Seller.LastName}"));
+
+            CreateMap<Product, SoldProductDTO>()
+                .ForMember(dest => dest.BuyerFirstName, y => y.MapFrom(src => src.Buyer.FirstName))
+                .ForMember(dest => dest.BuyerLastName, y => y.MapFrom(src => src.Buyer.LastName));
+
+            CreateMap<User, UserOutputDTO>()
+                .ForMember(dest => dest.SoldProducts, y => y.MapFrom(src => src.ProductsSold));
+        }
+    }
+}
