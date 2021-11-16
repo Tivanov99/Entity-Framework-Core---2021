c8e1dade03559200a3a4ae729dfe4361a68b53e6
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-16 10:33:05 +0200

    Add files via upload

---
+++ JavaScript Object Notation - JSON/15. Export Cars From Make Toyota/CarDealerProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<Supplier, SupplierDTO>();
+
+            CreateMap<Customer, CustomerDTOOutput>()
+                .ForMember(dest=>dest.BirthDate,y=>y.MapFrom(src=>src.BirthDate.ToString(@"dd\/MM\/yyyy")));
+
+            CreateMap<Car, CarOutputDTO>();
+        }
+    }
+}
