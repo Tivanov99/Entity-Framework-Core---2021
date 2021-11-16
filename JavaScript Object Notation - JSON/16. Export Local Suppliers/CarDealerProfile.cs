de5abcebeb4c5d965ccb82073862cc8db4473f81
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-16 10:44:03 +0200

    Add files via upload

---
+++ JavaScript Object Notation - JSON/16. Export Local Suppliers/CarDealerProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<Supplier, SupplierDTO>();
+
+            CreateMap<Customer, CustomerDTOOutput>()
+                .ForMember(dest => dest.BirthDate, y => y.MapFrom(src => src.BirthDate.ToString(@"dd\/MM\/yyyy")));
+
+            CreateMap<Car, CarOutputDTO>();
+
+            CreateMap<Supplier, SupplierOutputDTO>()
+                .ForMember(dest => dest.PartsCount, y => y.MapFrom(src => src.Parts.Count));
+        }
+    }
+}
