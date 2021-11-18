5dd304fdd2dd3a3de390dd5ea35bb179907c6537
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-18 18:50:16 +0200

    Add files via upload

---
+++ Extensible Markup Language - XML/16. Export Local Suppliers/CarDealerProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CarDealer.Dto;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<Car, CarOutputDTO>();
+
+            CreateMap<Car, BmwDTO>();
+
+            CreateMap<Supplier, SuppliersDTO>()
+                .ForMember(dest => dest.PartsCount, y => y.MapFrom(src => src.Parts.Count));
+        }
+    }
+}
