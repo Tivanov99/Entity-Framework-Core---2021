9f67df001e08646dd2668b525dc5fea1069fb19c
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-18 12:49:59 +0200

    Add files via upload

---
+++ Extensible Markup Language - XML/06. Export Sold Products/Dtos/Export/UserOutputDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("User")]
+    public class UserOutputDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlArray("soldProducts")]
+        public List<SoldProductsDTO> SoldProducts { get; set; }
+    }
+}
---
+++ Extensible Markup Language - XML/06. Export Sold Products/ProductShopProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProductShop.Dtos.Export;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<Product, ProductOutputDTO>()
+                .ForMember(dest => dest.BuyerFullName, y => y.MapFrom(src => $"{src.Buyer.FirstName} {src.Buyer.LastName}"));
+
+            CreateMap<User, UserOutputDTO>()
+                .ForMember(dest => dest.SoldProducts, y => y.MapFrom(src => src.ProductsSold));
+        }
+    }
+}
