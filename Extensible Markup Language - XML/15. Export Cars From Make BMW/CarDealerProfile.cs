213f54eff154213ff96919247bd0cf0856cbdd34
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-18 18:34:31 +0200

    Add files via upload

---
+++ Extensible Markup Language - XML/15. Export Cars From Make BMW/CarDealerProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CarDealer.Dto;
+using CarDealer.DTO;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<Car, CarOutputDTO>();
+
+            CreateMap<Car, BmwDTO>();
+        }
+    }
+}
