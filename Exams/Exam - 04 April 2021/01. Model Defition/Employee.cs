204c44f6110a7579285983215d4dc50d4e9d5cec
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2021-11-22 21:52:54 +0200

    Add files via upload

---
+++ Exams/Exam - 04 April 2021/01. Model Defition/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmployeesTasks= new HashSet<EmployeeTask>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        [RegularExpression(@"[a-zA-Z]+([0-9])|[a-zA-Z]")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")]
+        public string Phone { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
